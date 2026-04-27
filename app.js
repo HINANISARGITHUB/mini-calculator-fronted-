552ba02f3a571faccc506ed8e8e12d489387098e
Hina Nisar <160288150+HINANISARGITHUB@users.noreply.github.com>
2026-04-27 10:54:00 +0500

    Restore calculate function with updated input and URL
    Uncommented the calculate function and updated input element ID and fetch URL.

--- app.js
+++ app.js
@@ -1,38 +1,3 @@
-// function calculate() {
-//     let input = document.getElementById("input").value;
-//     fetch( , {          // backend deployment link
-//         method: 'POST',
-//         headers: {
-//             "Content-Type": "application/json"
-//         }
-
-// }
-
-
-// function calculate() {
-//     let input = document.getElementById("input").value;
-
-//     fetch("", {   // agar deploy ho to yahan apna live link lagana backed ka
-//         method: 'POST',
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify({ expression: input })
-//     })
-//     .then(res => res.json())
-//     .then(data => {
-//         if (data.result !== undefined) {
-//             document.getElementById("result").innerText = "Result: " + data.result;
-//         } else {
-//             document.getElementById("result").innerText = data.Error;
-//         }
-//     })
-//     .catch(err => {
-//         document.getElementById("result").innerText = "Error connecting server";
-//     });
-// }
-
-
 function calculate() {
     let input = document.getElementById("calc-input").value;
 
